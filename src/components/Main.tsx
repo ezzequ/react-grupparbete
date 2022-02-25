edf7d0b6d747479509dd63f649a756bad0d9203e
linnea <linnea.albertsson@gmail.com>
2022-02-25 09:52:35 +0100

    Add separate component for photos Fixes #18

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,53 +1,23 @@
 import { Component } from "react";
+import PhotoCard from "./PhotoCard";
 
 
+interface Props {};
 
 
-interface ImageData {
-    alt_description: "white modern cement building under blue sky";
-    id: "RFDP7_80v5A";
-    liked_by_user: false;
-    urls: {
-        full: string;
-        raw: string;
-        regular: string;
-    }  
-}
-
-interface Props {}
-interface State {
-    imagesData: any[];
-}
-
-
-class Main extends Component<Props, State> {
+class Main extends Component {
     constructor(props: Props) {
         super(props);
         this.state = { imagesData: [] };
     }
 
-    async componentDidMount() {
-        const url = 'https://api.unsplash.com/search/photos?page=10&query=landscape';
-        const response = await fetch(url, {
-            headers: {
-                "authorization": "Client-ID atOI9pA6vM7-48sURp4sfveE-vt7pSu99waKjQ5GWUc"
-            }
-        });
-        const data: ImageData[] = (await response.json()).results; 
-        this.setState({ imagesData: data});
-    }
+    
     
     render() {
         console.log(this.state);
       return(
-          
         <main className="fr w-100 pa2">
-            {this.state.imagesData.map(imageData => 
-                <div className="bg-light-grey br1 grow ma1
-                shadow-5 fr w-40  br3">
-                    <img key={imageData.id} src={imageData.urls.regular} alt={imageData.alt_description} className="br3 w-100" />
-                </div>
-                )}
+           <PhotoCard />
         </main>
         );  
     }
---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,60 @@
+import { Component, CSSProperties } from "react"
+
+interface Props {}
+interface State {
+    imagesData: any[];
+}
+
+
+interface ImageData {
+    alt_description: "white modern cement building under blue sky";
+    id: "RFDP7_80v5A";
+    liked_by_user: false;
+    urls: {
+        full: string;
+        raw: string;
+        regular: string;
+    }  
+}
+
+
+class PhotoCard extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { imagesData: [] };
+    }
+
+
+    async componentDidMount() {
+        const url = 'https://api.unsplash.com/search/photos?page=10&per_page=30&query=blackandwhite';
+        const response = await fetch(url, {
+            headers: {
+                "authorization": "Client-ID atOI9pA6vM7-48sURp4sfveE-vt7pSu99waKjQ5GWUc"
+            }
+        });
+        const data: ImageData[] = (await response.json()).results; 
+        this.setState({ imagesData: data});
+    }
+
+
+    render() {
+        return(
+            <div className="photos-container">
+                {this.state.imagesData.map(imageData => 
+                    <div style={photoCardStyle} className="bg-light-grey br1 grow ma1 shadow-5 fr w-40  br3">
+                        <img key={imageData.id} src={imageData.urls.regular} alt={imageData.alt_description} className="br3 w-100" />
+                    </div>
+                )}
+            </div>
+        )
+    }
+
+
+}
+
+const photoCardStyle: CSSProperties = {
+    display: "flex",
+    flexWrap: "wrap"
+}
+
+export default PhotoCard;
